f5aac14964f44cf3aa5d9871d33101e40deccdaa
Viktoriya Vinislavskaya <viktoriyavinislavskaya@gmail.com>
2022-09-05 15:25:21 +0300

    first commit

---
+++ INTRO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  INTRO
+//
+//  Created by Виктория Виниславская on 05.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ INTRO/INTROApp.swift
@@ -0,0 +1,17 @@
+//
+//  INTROApp.swift
+//  INTRO
+//
+//  Created by Виктория Виниславская on 05.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct INTROApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
